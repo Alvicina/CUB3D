93e96381998b30f76f8b2e6d1bcec129e6a90cd4
Alejandro Vicinay Estrade <alvicina@c3r4s4.42urduliz.com>
2024-02-21 13:44:24 +0100

    pasar norma y validar mapa

--- mandatory/color_validation.c
+++ mandatory/color_validation.c
@@ -6,29 +6,17 @@
 /*   By: alvicina <alvicina@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 13:20:26 by alvicina          #+#    #+#             */
-/*   Updated: 2024/02/21 13:22:16 by alvicina         ###   ########.fr       */
+/*   Updated: 2024/02/21 13:43:12 by alvicina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
-static int	is_color_valid(char *str)
+static int	value_valid(char *str, size_t *flag, char **num, int *num_atoi)
 {
 	size_t	i;
 	size_t	j;
-	size_t	flag;
-	int		num_atoi;
-	char	*num;
-
-	i = 0;
-	flag = 0;
-	num_atoi = 0;
-	while (str[i])
-	{
-		if (str[i] != ' ' && str[i] != ',' && !(str[i] >= 48 && str[i] <= 57))
-			return (EXIT_FAILURE);
-		i++;
-	}
+	
 	i = 0;
 	j = 0;
 	while (str[i])
@@ -38,19 +26,48 @@ static int	is_color_valid(char *str)
 		j = i;
 		while (str[j] >= 48 && str[j] <= 57)
 			j++;
-		flag++;
-		if (flag > 3)
+		(*flag)++;
+		if (*flag > 3)
 			return (EXIT_FAILURE);
-		num = ft_substr(str, i, j - i);
-		if (num == NULL)
+		*num = ft_substr(str, i, j - i);
+		if (*num == NULL)
 			return (perror(0), EXIT_FAILURE);
-		num_atoi = ft_atoi(num);
-		if (num_atoi > 255)
+		*num_atoi = ft_atoi(*num);
+		if (*num_atoi > 255)
 			return (EXIT_FAILURE);
-		ft_printf("%s\n", num);
-		free(num);
+		free(*num);
 		i = j;
 	}
+	return (EXIT_SUCCESS);
+}
+
+static int	chars_valid(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] != ' ' && str[i] != ',' && !(str[i] >= 48 && str[i] <= 57))
+			return (EXIT_FAILURE);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
+
+static int	is_color_valid(char *str)
+{
+	size_t	flag;
+	char	*num;
+	int		num_atoi;
+
+	flag = 0;
+	num = NULL;
+	num_atoi = 0;
+	if (chars_valid(str))
+		return (EXIT_FAILURE);
+	if (value_valid(str, &flag, &num, &num_atoi))
+		return (EXIT_FAILURE);
 	if (flag != 3)
 		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
