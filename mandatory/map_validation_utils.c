9f009a4cd98ec3ac43aa99024e30959a5237763d
Alejandro Vicinay Estrade <alvicina@c4r3s6.42urduliz.com>
2024-02-26 16:13:41 +0100

    gestionar espacios por detras del mapa

--- mandatory/map_validation_utils.c
+++ mandatory/map_validation_utils.c
@@ -6,7 +6,7 @@
 /*   By: alvicina <alvicina@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 11:56:10 by alvicina          #+#    #+#             */
-/*   Updated: 2024/02/26 13:28:25 by alvicina         ###   ########.fr       */
+/*   Updated: 2024/02/26 16:11:12 by alvicina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,64 @@
 
 int	what_around_whitespace(t_data *data, size_t i, size_t j)
 {
+	size_t	map_lines;
+
+	map_lines = 0;
+	while (data->map_only[map_lines])
+		map_lines++;
 	if (i == 0)
 	{
+		/*if (j > ft_strlen(data->map_only[i + 1]))
+			return (EXIT_SUCCESS);*/
 		if (j == 0)
 		{
-			if ((data->map_only[i + 1][j] != '1' && data->map_only[i + 1][j] != ' ')
-			|| (data->map_only[i][j + 1] != '1' && data->map_only[i][j + 1] != ' '))
-				return (EXIT_FAILURE);
+			if (j < ft_strlen(data->map_only[i]) - 1 && i < map_lines - 1)
+			{
+				if ((data->map_only[i + 1][j] != '1' && data->map_only[i + 1][j] != ' ')
+				|| (data->map_only[i][j + 1] != '1' && data->map_only[i][j + 1] != ' '))
+					return (EXIT_FAILURE);
+			}
 		}
 		else
 		{
-			if ((data->map_only[i + 1][j] != '1' && data->map_only[i + 1][j] != ' ')
-			|| (data->map_only[i][j + 1] != '1' && data->map_only[i][j + 1] != ' ')
-			|| (data->map_only[i][j - 1] != '1' && data->map_only[i][j - 1] != ' '))
-				return (EXIT_FAILURE);	
+			if (j < ft_strlen(data->map_only[i]) - 1 && i < map_lines - 1)
+			{
+				if ((data->map_only[i + 1][j] != '1' && data->map_only[i + 1][j] != ' ')
+				|| (data->map_only[i][j + 1] != '1' && data->map_only[i][j + 1] != ' ')
+				|| (data->map_only[i][j - 1] != '1' && data->map_only[i][j - 1] != ' '))
+					return (EXIT_FAILURE);
+			}
 		}
 	}
 	else
 	{
+		/*if (j > ft_strlen(data->map_only[i + 1]))
+			return (EXIT_SUCCESS);*/
 		if (j == 0)
 		{
-			if ((data->map_only[i + 1][j] != '1' && data->map_only[i + 1][j] != ' ')
-			|| (data->map_only[i - 1][j] != '1' && data->map_only[i - 1][j] != ' ')
-			|| (data->map_only[i][j + 1] != '1' && data->map_only[i][j + 1] != ' '))
-				return (EXIT_FAILURE);
+			if (j < ft_strlen(data->map_only[i]) - 1 && i < map_lines - 1)
+			{
+				if ((data->map_only[i + 1][j] != '1' && data->map_only[i + 1][j] != ' ')
+				|| (data->map_only[i - 1][j] != '1' && data->map_only[i - 1][j] != ' ')
+				|| (data->map_only[i][j + 1] != '1' && data->map_only[i][j + 1] != ' '))
+					return (EXIT_FAILURE);
+			}
 		}
 		else
 		{
 			printf("j: %zu i: %zu\n", j, i);
-			if ((data->map_only[i + 1][j] != '1' && data->map_only[i + 1][j] != ' ')
-			|| (data->map_only[i - 1][j] != '1' && data->map_only[i - 1][j] != ' ')
-			|| (data->map_only[i][j + 1] != '1' && data->map_only[i][j + 1] != ' ')
-			|| (data->map_only[i][j - 1] != '1' && data->map_only[i][j - 1] != ' '))
-				return (EXIT_FAILURE);
+			printf("ft_strlen: %zu map_lines: %zu\n", ft_strlen(data->map_only[i]), map_lines);
+			if (j < ft_strlen(data->map_only[i]) - 1 && i < map_lines - 1)
+			{
+				if ((data->map_only[i + 1][j] != '1' && data->map_only[i + 1][j] != ' ')
+				|| (data->map_only[i - 1][j] != '1' && data->map_only[i - 1][j] != ' ')
+				|| (data->map_only[i][j + 1] != '1' && data->map_only[i][j + 1] != ' ')
+				|| (data->map_only[i][j - 1] != '1' && data->map_only[i][j - 1] != ' '))
+				{
+					
+					return (EXIT_FAILURE);
+				}
+			}
 		}
 	}
 	return (EXIT_SUCCESS);
